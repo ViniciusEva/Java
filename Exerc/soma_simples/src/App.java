63ba60821814508bc92d391eca643cad768b3cdf
ViniciusEva <vinicius.evangelista.ds@gmail.com>
2025-09-03 10:06:08 -0300

    Add files via upload

---
+++ Exerc/soma_simples/src/App.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+         Scanner sc = new Scanner(System.in);
+
+         int a = sc.nextInt();
+         int b = sc.nextInt();
+
+         int resultado = a + b;
+
+         System.out.println("SOMA = "+resultado);
+
+         sc.close();
+    }
+}
